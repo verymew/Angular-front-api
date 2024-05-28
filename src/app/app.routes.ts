f65325b0b64ce553f0f844a8e6c4d921bdad1c40
Verymew <111842541+verymew@users.noreply.github.com>
2024-05-27 23:01:51 -0300

    Rotas adicionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TesteComponent } from './teste/teste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TesteComponent }
+];
