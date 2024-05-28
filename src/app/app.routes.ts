f6974884a4843a81fca3497b9c7b66a7f457c318
Verymew <111842541+verymew@users.noreply.github.com>
2024-05-27 23:16:25 -0300

    ;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TesteComponent } from './teste/teste.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 export const routes: Routes = [
-    { path: '', component: TesteComponent }
+    { path: '', component: TesteComponent },
+    { path: '**', component: PageNotFoundComponentComponent }
 ];
